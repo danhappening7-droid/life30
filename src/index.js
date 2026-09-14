23cb8a1b1b73dc8178d4ede88ba92f10b6a21f8e
danhappening7-droid <danhappening7@gmail.com>
2026-09-14 15:01:09 +0900

    Share page: update CTA lead text (two lines)

--- src/index.js
+++ src/index.js
@@ -195,7 +195,7 @@ ${tile("私が望む未来", W, "want")}
 ${tile("こうなっちゃうだろう未来", L, "likely")}
 </div>
 <div class="cta">
-<p class="lead">超知能（ASI）と人間が共存する12のシナリオと、その分岐点を見にいく</p>
+<p class="lead">超知能（ASI）の進化と人間が交錯する<br>12のシナリオと、その分岐点を見にいく</p>
 <a class="big" href="${ORIGIN}/"><img src="${ORIGIN}/favicon.svg" alt="" width="34" height="34"><span>Go to 12futures.jp</span></a>
 <p class="note">LIFE 3.0（マックス・テグマーク）の将来シナリオを、インタラクティブな分岐図と日本語解説で読めるページ。登録不要</p>
 </div>
